fd9a9efca97b261755a3e9a4872a3f1af60a1bf1
Sara Katharina Horvat <01310623@edu.campus02.at>
2026-01-22 11:58:24 +0100

    explanatory comments and zeroAmountProvision added

--- src/src/main/java/campus02/iwi/horvat/Provisonsberechnung/KeineProvision.java
+++ src/src/main/java/campus02/iwi/horvat/Provisonsberechnung/KeineProvision.java
@@ -1,6 +1,9 @@
 package campus02.iwi.horvat.Provisonsberechnung;
 
 public class KeineProvision implements Provisionsberechnung {
+
+    //Fall abdecken, dass keine Provision berechnet werden soll
+
     @Override
     public double berechneProvision(Mitarbeiter mitarbeiter) {
         System.out.println("Hinweis: Kein Anspruch auf Provision bei diesem MA.");
